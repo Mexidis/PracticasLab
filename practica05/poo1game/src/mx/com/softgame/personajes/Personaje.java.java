e452a5aab021b1e7bcb4e83281be07c3831c78bf
Toromax <mexidis11@gmail.com>
2021-09-15 09:36:46 -0500

    Paso 4, 5

---
+++ practica05/poo1game/src/mx/com/softgame/personajes/Personaje.java.java
@@ -0,0 +1,47 @@
+//hora de inicio 8:10 AM
+package mx.com.softgame.personajes;
+public class Personaje{
+
+	//atributos
+	private String nombre;
+	private int edad;
+
+	//constructores (no lleva tipo de dato 
+	// y tiene el nombre de la clase)
+	public Personaje(String nombre){
+		//"this" es para el atributo
+		this.nombre = nombre;
+		//sin "this" es para la var. local (dentro del )
+		edad = 0;
+	}
+
+	//metodos (llevan tipo de dato y puede ser 
+	// cualquier otro nombre)
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+nombre);
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public boolean setEdad(int edad){
+		if (!(edad > 0 && edad <120)){
+			return false;
+		}
+			else{
+				this.edad = edad;
+				return true;
+			}
+	}	
+	
+}
+
+//Hora de finalización 9:09 AM
+
+
+
