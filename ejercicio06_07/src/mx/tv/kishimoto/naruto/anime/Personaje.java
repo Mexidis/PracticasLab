a9e81291710ef15e915f88912b3a6765ff2e6796
Toromax <mexidis11@gmail.com>
2021-09-17 10:14:46 -0500

    Pasos 7, 8

--- ejercicio06_07/src/mx/tv/kishimoto/naruto/anime/Personaje.java
+++ ejercicio06_07/src/mx/tv/kishimoto/naruto/anime/Personaje.java
@@ -5,25 +5,42 @@ public class Personaje{
 
 	//atributos
 	private String nombre;
-	//(int)(Math.random()*501)
-	private int poder = 0 | 500;
-	private int edad;
-
+	private int poder;
+	
 	//constructores
 
 	public Personaje(String nombre, int poder){
 		this.nombre = nombre;
 		this.poder = poder;
+		if(poder > 500){
+			this.poder = 500;
+			return;
+		}
+		if (poder < 0) {
+			this.poder = 0;
+			return;
+		}
+		this.poder = poder;
 	}
 
 	//metodos 
 
-	public void setPoder(int num){
-		num = num + poder;
+	public void setPoder(int poder){
+		int resultado = this.poder + poder;
+		if(resultado > 500){
+			this.poder = 500;
+			return;
+		}
+		if(resultado < 0){
+			this.poder = 0;
+			return;
+		}
+		this.poder = resultado;
 	}
 
-	public void getDatos(){
-		System.out.println("\t"+nombre+"\t"+poder);
+	public String getDatos(){
+		String alaa = nombre+"\t"+poder;
+		return alaa;
 	}
 
 	public void setNombre(String nombre){
@@ -33,26 +50,7 @@ public class Personaje{
 	}
 	public String getNombre(){
 		return nombre;
-	}
-	public void saludar(){
-		System.out.println("Hola Alumno de POO "+nombre);
-	}
-	public int getEdad(){
-		return edad;
-	}
-	public boolean setEdad(int edad){
-		if (!(edad > 0 && edad <120)){
-			return false;
-		}
-			else{
-				this.edad = edad;
-				return true;
-			}
-	}
-	public void getDetalle() {
-		System.out.println("Personaje "+nombre+"\t con edad "+edad);
 	}	
-	
 }
 
 //Hora de finalización 
--- ejercicio06_07/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
+++ ejercicio06_07/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
@@ -1,6 +1,18 @@
 //hora de inicio 8:03 AM
 package mx.tv.kishimoto.naruto.pruebas;
+import mx.tv.kishimoto.naruto.anime.*;
 
 public class Escena{
-	
+	public static void main(String[] args) {
+		Personaje naruto = new Personaje("Naruto", 1000);
+		Personaje sasuke = new Personaje("Sasuke", -10);
+		Personaje sakura = new Personaje("Sakura", 300);
+
+		System.out.println("---");
+		System.out.println(naruto.getDatos());
+		System.out.println(sasuke.getDatos());
+		System.out.println(sakura.getDatos());
+		System.out.println("---");
+
+	}
 }
