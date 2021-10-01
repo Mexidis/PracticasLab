95d42845340be4da5644a73c4faddc9f790eae51
Toromax <mexidis11@gmail.com>
2021-10-01 09:47:10 -0500

    Igual que el anterior 9:46

---
+++ practica08/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
@@ -0,0 +1,26 @@
+package mx.com.softgame.poo1game.personajes.buenos;
+import mx.com.softgame.poo1game.personajes.Personaje;
+public class Planta extends Personaje{
+	private char escudo;
+
+	public Planta(String nombre, int vida, char escudo){
+		super(nombre, vida);
+		this.escudo = escudo;
+	}
+	public Planta(String nombre, char escudo){
+		super(nombre);
+		this.escudo = escudo;
+	}
+	public Planta(String nombre, int vida){
+		super(nombre);	
+		this.escudo = 'A';
+	}
+	public Planta(String nombre){
+		super(nombre);
+		this.escudo = 'A';
+	}
+
+	public char getEscudo(){
+		return escudo;
+	}
+}
