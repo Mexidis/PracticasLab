5b6c8f20095ca7b4ecee96869ef9062cd658ad0d
Toromax <mexidis11@gmail.com>
2021-10-01 11:05:22 -0500

    Prac08 Paso 9 a las 11.05

---
+++ practica08/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -0,0 +1,19 @@
+package mx.com.softgame.poo1game.pruebas;
+import mx.com.softgame.poo1game.personajes.*;
+
+public class PruebaHerencia{
+	public static void main(String[] args) {		
+		Personaje per01 = new Personaje("Miguel", 99);
+		Personaje per02 = new Personaje("Sarahí");
+		Planta plan01 = new Planta("Angel", 10, 'B');
+		Planta plan02 = new Planta("Giovana", 50);
+		Planta plan03 = new Planta("Antonio", 'C');
+		Planta plan04 = new Planta("Valeria");
+		Zombie zom01 = new Zombie("Alan", 80, false);
+		Zombie zom02 = new Zombie("Mayela", true);
+		Zombie zom03 = new Zombie("Carlos");
+
+		Personaje[] personajes = new Personaje;
+
+	}
+}
