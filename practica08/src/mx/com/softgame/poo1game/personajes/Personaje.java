42f8dc2fc968d248def452a4506d398b916c4c2b
Toromax <mexidis11@gmail.com>
2021-10-01 09:07:23 -0500

    Prac 08 paso4  9:00

--- practica08/src/mx/com/softgame/poo1game/personajes/Personaje.java
+++ practica08/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -9,21 +9,24 @@ public class Personaje{
 	public Personaje(String nombre, int vida){
 		this.nombre = nombre;
 		this.vida = vida;
-		if (vida < 0) {
-			this.vida = 0;
-			return;
-		}
-		this.vida = vida;
 	}
 	public Personaje(String nombre){
 		this.nombre = nombre;
-		vida = 3;
+		this.vida = 3;
 	}
 	public void decVida(){
+		if (this.vida < 0) {
+			this.vida = this.vida;
+			return;
+		}
 		this.vida -= 1;
 	}
-	public int decVida(int dec){
-		this.vida - dec = vida;
+	public void decVida(int dec){
+		if (this.vida < 0) {
+			this.vida = this.vida;
+			return;
+		}
+		this.vida = this.vida - dec;
 	}
 	public int getVida(){
 		return vida;
