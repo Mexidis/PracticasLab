75a5de0455a409a35a35ae833b81f61a802eb0d8
Toromax <mexidis11@gmail.com>
2021-10-08 09:15:40 -0500

    Practica09 9:15

--- practica09/src/edu/uaz/ingsoft/appcovid/universitarios/maestros/Maestro.java
+++ practica09/src/edu/uaz/ingsoft/appcovid/universitarios/maestros/Maestro.java
@@ -1,7 +1,7 @@
 package edu.uaz.ingsoft.appcovid.universitarios.maestros;
 import edu.uaz.ingsoft.appcovid.universitarios.*;
 
-public class Maestro extends universitario{
+public class Maestro extends Universitario{
 
 	private float sueldo;
 
@@ -11,11 +11,15 @@ public class Maestro extends universitario{
 	}
 
 	public Maestro(String nombre){
-		this.sueldo = 1000.00f;
+		this(nombre, 1000.00f);
 	}
 
 	public float sueldo(){
-		if ()
+		if (this.sueldo <= 20000.00f && this.sueldo >= 100.00f){
+			return sueldo;
+		} else{
+			return sueldo = 1000.00f;
+		}
 	}
 }
 	
