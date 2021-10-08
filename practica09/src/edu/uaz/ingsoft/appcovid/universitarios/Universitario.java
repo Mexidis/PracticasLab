6f43c5824543a2379b434bfde88ceccb85d9cf46
Toromax <mexidis11@gmail.com>
2021-10-08 08:02:20 -0500

    Practica09 8:02

--- practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
+++ practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
@@ -1,3 +1,11 @@
+package edu.uaz.ingsoft.appcovid.universitarios;
+
 public class Universitario{
-	
+	/*private String nombre;
+
+	public Universitario(String nombre){
+		this.nombre = nombre;
+	}
+*/
+
 }
