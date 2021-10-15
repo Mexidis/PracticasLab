896b3b9e376a2fe4c5fcec3bff242889250da9b1
Toromax <mexidis11@gmail.com>
2021-10-15 08:30:17 -0500

    Practica 10 8:30

---
+++ practica10/src/edu/uaz/ingsoft/appcovid/utils/Tablero.java
@@ -0,0 +1,16 @@
+package edu.uaz.ingsoft.appcovid.universitarios.utils;
+
+public class Tablero{
+	private static int poss = -1;
+	public static final int MAX = 5;
+	private Universitario personas [] = new Universitario[MAX];
+
+	private Tablero(){
+
+	}
+
+	public static void mostrar(){
+		System.out.print(Universitario);
+	}
+
+}
