b2662ab72ce66a10de42d5d9c49f371483eaf544
Toromax <mexidis11@gmail.com>
2021-10-15 11:00:49 -0500

    Practica10 11:00

--- practica10/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaUniv.java
+++ practica10/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaUniv.java
@@ -6,13 +6,14 @@ import edu.uaz.ingsoft.appcovid.utils.*;
 
 public class PruebaUniv{
 	public static void main(String[] args) {
-		insertar(new Universitario("Jose Miguel"));
-		insertar(new Maestro("Giovanna Esmeralda", 10f), 4);
-		insertar(new Alumno("Hanna Jimena", 80));
-		insertar(new Universitario("Saul Alfonso"));
-		insertar(new Maestro("Edwin Brayan", 5000f), 6);
-		insertar(new Alumno("Esther Sarahi", 5));
+		Tablero.insertar(new Universitario("Jose Miguel"));
+		// insertar(new Maestro("Giovanna Esmeralda", 10f), 4);
+		// insertar(new Alumno("Hanna Jimena", 80));
+		// insertar(new Universitario("Saul Alfonso"));
+		// insertar(new Maestro("Edwin Brayan", 5000f), 6);
+		// insertar(new Alumno("Esther Sarahi", 5));
 
-		mostrar()
+		System.out.println(mostrar());
+
 	}
 }
