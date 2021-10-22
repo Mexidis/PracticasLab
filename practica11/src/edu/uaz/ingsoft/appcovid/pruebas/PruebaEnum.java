9fcdfd79729ed4bfbe15eb5bc10cd9d38b23f6fa
Toromax <mexidis11@gmail.com>
2021-10-22 11:46:07 -0500

    Práctica11 11:45

--- practica11/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaEnum.java
+++ practica11/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaEnum.java
@@ -9,8 +9,23 @@ public class PruebaEnum{
 		for (int i = 0 ; i < 5 ; i++) {
 			System.out.println(a);
 
-			private getCalifAleatoria(){
-				int rand
+			private Calificacion getCalifAleatoria(){
+				int rand = (int)(Math.random()*10);
+				switch (rand) {
+					case 0: return Calificacion.CERO;
+					case 1: return Calificacion.UNO;
+					case 2: return Calificacion.DOS;
+					case 3: return Calificacion.TRES;
+					case 4: return Calificacion.CUATRO;
+					case 5: return Calificacion.CINCO;
+					case 6: return Calificacion.SEIS;
+					case 7: return Calificacion.SIETE;
+					case 8: return Calificacion.OCHO;
+					case 9: return Calificacion.NUEVE;
+					case 10: return Calificacion.DIEZ;
+					a.setCalificacion(c, indice);
+					System.out.println(a);
+				}
 			}
 		}
 	}
