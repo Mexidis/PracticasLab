4d3b2b5682b15e2b7fae268f75793cad0c107fa4
Toromax <mexidis11@gmail.com>
2021-10-29 09:01:13 -0500

    Práctica12 9:00

--- practica12/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaAbstract.java
+++ practica12/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaAbstract.java
@@ -6,12 +6,12 @@ import edu.uaz.ingsoft.appcovid.utils.*;
 
 public class PruebaAbstract {
 	public static void main(String[] args) {
-		Universitario[] universitarios;
-		universitarios = new Universitario[5];
-		u1 = new Universitario("Aldo"); 
-		m1 = new Maestro("Carolina", 10000f);
-		m2 = new Maestro("Jorge",20000f);
-		a1 = new Alumno("Miriam");
-		a2 = new Alumno("Patrick");
+		Universitario[] universitarios = {
+			// new Universitario("Aldo"), 
+			 new Maestro("Carolina", 10000f),
+			 new Maestro("Jorge",20000f),
+			 new Alumno("Miriam"),
+			 new Alumno("Patrick"),
+		};
 	}
 }
