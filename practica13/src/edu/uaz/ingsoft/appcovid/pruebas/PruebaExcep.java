8be1c0e45d450fbe5ea328653b795dfda2f20a31
Toromax <mexidis11@gmail.com>
2021-11-05 08:16:23 -0600

    Práctica13 8:15

--- practica13/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaExcep.java
+++ practica13/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaExcep.java
@@ -5,11 +5,15 @@ import edu.uaz.ingsoft.appcovid.universitarios.alumnos.*;
 import edu.uaz.ingsoft.appcovid.utils.*;
 public class PruebaExcep {
 	public static void main(String[] args) {
-		Tablero.insertar(new Maestro("Cristian", 1000.f));
-		Tablero.insertar(new Maestro("Jorge", 100000f));
-		Tablero.insertar(new Alumno("Valeria"));
-		Tablero.insertar(new Maestro("Federico", 5f));
-		Tablero.insertar(new Alumno("Hanna"));
-		Tablero.insertar(new Alumno("Miriam"));
+		try{
+			Tablero.insertar(new Maestro("Cristian", 1000.f));
+			Tablero.insertar(new Maestro("Jorge", 100000f));
+			Tablero.insertar(new Alumno("Valeria"));
+			Tablero.insertar(new Maestro("Federico", 5f));
+			Tablero.insertar(new Alumno("Hanna"));
+			Tablero.insertar(new Alumno("Miriam"));
+		} catch (TableroException te){
+			
+		}
 	}
 }
