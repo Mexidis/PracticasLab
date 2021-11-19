adcb5b82d222ce27601be8c6f40f02923fa3c29d
Toromax <mexidis11@gmail.com>
2021-11-19 11:45:11 -0600

    Práctica15 11:45

--- practica15/src/com/estadistica/stata/pruebas/PruebaIO.java
+++ practica15/src/com/estadistica/stata/pruebas/PruebaIO.java
@@ -4,6 +4,7 @@ import java.io.File;
 public class PruebaIO {
 	public static void main(String[] args) {
 		File home = System.getProperty("user.home");
-		String path = home+System.getProperty("file.separator");
+		String path = home+ System.getProperty("file.separator");
 	}
+	
 }
