5699370f439301d12341a75bcc2814248128d1b8
Toromax <mexidis11@gmail.com>
2021-11-19 12:15:14 -0600

    Práctica15 12:00

--- practica15/src/com/estadistica/stata/pruebas/PruebaIO.java
+++ practica15/src/com/estadistica/stata/pruebas/PruebaIO.java
@@ -1,10 +1,22 @@
 package com.estadistica.stata.pruebas;
+import com.estadistica.stata.archivos.*;
 import java.io.File;
 
 public class PruebaIO {
 	public static void main(String[] args) {
-		File home = System.getProperty("user.home");
-		String path = home+ System.getProperty("file.separator");
+		File home = new File(System.getProperty("user.home"));
+		if (home.isFile()) {
+			System.out.println("No se puede continuar");
+			System.exit(-1);
+		}
+		String path = home.getAbsolutePath()+
+			System.getProperty("file.separator");
+		String[] contenido = home.list();
+		// for (Municipio entidadActual : municipios) {
+		// 	if (entidadActual.isFile() && entidadActual == "datos.info") {
+		// 		Acciones.procesar(path+entidadActual);
+		// 	}
+		// }
+		municipios = Acciones.getMunicipios();
 	}
-	
 }
