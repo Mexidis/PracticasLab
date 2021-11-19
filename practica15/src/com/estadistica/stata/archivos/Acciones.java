272ac4b50ce562647b3aabe3ee33fb101a657ded
Toromax <mexidis11@gmail.com>
2021-11-19 08:16:27 -0600

    Práctica15 8:15

---
+++ practica15/src/com/estadistica/stata/archivos/Acciones.java
@@ -0,0 +1,9 @@
+package src.com.estadistica.stata;
+
+public class Acciones{
+	private static ArrayList<Municipio> municipios = new ArrayList();
+
+	public boolean esArchivo(String s){
+		
+	}
+}
--- practica15/src/com/estadistica/stata/archivos/Municipio.java
+++ practica15/src/com/estadistica/stata/archivos/Municipio.java
@@ -12,7 +12,7 @@ public class Municipio{
 	}
 
 	public short getId(){
-		return getId;
+		return id;
 	}
 	public String getName(){
 		return name;
@@ -22,6 +22,6 @@ public class Municipio{
 	}
 
 	public String toString(){
-		return "{"+id+"\t"+name+"\t"+population+"}"
+		return "{"+id+"\t"+name+"\t"+population+"}";
 	}
 }
