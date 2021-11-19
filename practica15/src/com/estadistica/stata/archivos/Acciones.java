137ad918ad3e6fb58c63d05827f05dbf90a5a51d
Toromax <mexidis11@gmail.com>
2021-11-19 08:30:48 -0600

    Práctica15 8:30

--- practica15/src/com/estadistica/stata/archivos/Acciones.java
+++ practica15/src/com/estadistica/stata/archivos/Acciones.java
@@ -1,9 +1,20 @@
 package src.com.estadistica.stata;
+import java.io.ArrayList;
 
 public class Acciones{
 	private static ArrayList<Municipio> municipios = new ArrayList();
 
 	public boolean esArchivo(String s){
 		
+		if(s.exists() && s.isDirectory()){
+			return true;			
+		}
+		return false;
 	}
+
+	// public void procesar(String s){
+	// 	municipios = new ArrayList<Municipio>();
+	// 	String line;
+	// 	line = buf
+	// }
 }
